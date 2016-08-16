031b0ab4cceb01f88228defc6f964e6a6d2df435
Daniel Chong <danielgq.chong@gmail.com>
2016-08-17 11:14:14 +1200

    translation structure

---
+++ translations/_translation.js
@@ -0,0 +1,103 @@
+import I18n from 'react-native-i18n';
+
+I18n.fallbacks = true;
+
+let languageCode = I18n.locale.substr(0,2);
+
+I18n.translations = {
+  en: require('./_en.json')
+};
+
+if (languageCode == 'ar') {
+  I18n.translations.ar = require('./ar.json');
+}
+if (languageCode == 'ca') {
+  I18n.translations.ca = require('./ca.json');
+}
+if (languageCode == 'cs') {
+  I18n.translations.cs = require('./cs.json');
+}
+if (languageCode == 'da') {
+  I18n.translations.da = require('./da.json');
+}
+if (languageCode == 'de') {
+  I18n.translations.de = require('./de.json');
+}
+if (languageCode == 'el') {
+  I18n.translations.el = require('./el.json');
+}
+if (languageCode == 'es') {
+  I18n.translations.es = require('./es.json');
+}
+if (languageCode == 'fi') {
+  I18n.translations.fi = require('./fi.json');
+}
+if (languageCode == 'fr') {
+  I18n.translations.fr = require('./fr.json');
+}
+if (languageCode == 'he') {
+  I18n.translations.he = require('./he.json');
+}
+if (languageCode == 'hi') {
+  I18n.translations.hi = require('./hi.json');
+}
+if (languageCode == 'hr') {
+  I18n.translations.hr = require('./hr.json');
+}
+if (languageCode == 'hu') {
+  I18n.translations.hu = require('./hu.json');
+}
+if (languageCode == 'id') {
+  I18n.translations.id = require('./id.json');
+}
+if (languageCode == 'it') {
+  I18n.translations.it = require('./it.json');
+}
+if (languageCode == 'ja') {
+  I18n.translations.ja = require('./ja.json');
+}
+if (languageCode == 'ko') {
+  I18n.translations.ko = require('./ko.json');
+}
+if (languageCode == 'ms') {
+  I18n.translations.ms = require('./ms.json');
+}
+if (languageCode == 'nb') {
+  I18n.translations.nb = require('./nb.json');
+}
+if (languageCode == 'nl') {
+  I18n.translations.nl = require('./nl.json');
+}
+if (languageCode == 'pl') {
+  I18n.translations.pl = require('./pl.json');
+}
+if (languageCode == 'pt') {
+  I18n.translations.pt = require('./pt.json');
+}
+if (languageCode == 'ro') {
+  I18n.translations.ro = require('./ro.json');
+}
+if (languageCode == 'ru') {
+  I18n.translations.ru = require('./ru.json');
+}
+if (languageCode == 'sk') {
+  I18n.translations.sk = require('./sk.json');
+}
+if (languageCode == 'sv') {
+  I18n.translations.sv = require('./sv.json');
+}
+if (languageCode == 'th') {
+  I18n.translations.th = require('./th.json');
+}
+if (languageCode == 'tr') {
+  I18n.translations.tr = require('./tr.json');
+}
+if (languageCode == 'uk') {
+  I18n.translations.uk = require('./uk.json');
+}
+if (languageCode == 'vi') {
+  I18n.translations.vi = require('./vi.json');
+}
+if (languageCode == 'zh') {
+  I18n.translations.zh = require('./zh.json');
+}
